d3f79a13f2e0dc1c234e4fd31aa594b604642ed7
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2024-06-28 00:45:50 +0300

    Update isValidId.js

--- helpers/isValidId.js
+++ helpers/isValidId.js
@@ -5,7 +5,8 @@ const HttpError = require('../helpers/HttpError')
 const isValidId = (req, res, next) => {
   const { id } = req.params
   if (!isValidObjectId(id)) {
-    next(HttpError(400, `${id} is not valid id`))
+    // next(HttpError(400, `${id} is not valid id`))
+    next(HttpError(404, ` not found`))
   }
   next()
 }
