df69a384934a76dc9aece7817fcbd9910e38fa11
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2024-06-27 20:58:32 +0300

    env

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose')
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000')
-})
+mongoose.set('strictQuery', true)
+
+// const DB_HOST =
+//   'mongodb+srv://alex:YySHYENjRdq369bV@cluster0.zygfoai.mongodb.net/contacts_reader?retryWrites=true&w=majority&appName=Cluster0'
+const { DB_HOST } = process.env
+
+mongoose
+  .connect(DB_HOST)
+
+  .then(() => app.listen(3000))
+
+  .catch((er) => {
+    console.log(er.message)
+    process.exit(1)
+  })
